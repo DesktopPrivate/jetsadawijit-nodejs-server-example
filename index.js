b15b5284143148d5f99208ae7df69f456459c94e
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-02-23 19:00:19 +0700

    set up

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        // Serve the HTML file
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end(`Error loading HTML file: ${err}`);
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
